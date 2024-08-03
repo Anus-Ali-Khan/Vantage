3ac2c7a67addbd32b8ce32f1670023a302e0ca08
Usama shakeel <princeusamaansari15@gmail.com>
2024-08-03 23:19:39 +0500

    home page completed

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,4 @@
-import Head from "next/head";
-import Image from "next/image";
-import { Inter } from "next/font/google";
-import styles from "@/styles/Home.module.css";
+import { Inter, Poppins } from "next/font/google";
 import Navbar from "@/components/Navbar";
 import ServicesSection from "@/components/ServicesSection";
 import ProjectCollabSection from "@/components/ProjectCollabSection";
@@ -9,19 +6,30 @@ import FeaturesSection from "@/components/FeaturesSection";
 import PaymentPlan from "@/components/PaymentPlan";
 import ExpertsSection from "@/components/ExpertsSection";
 import FooterSection from "@/components/FooterSection";
+import HeroSection from "@/components/HeroSection";
+import Carousel from "@/components/Carousel";
+import ProjectsSection from "@/components/ProjectsSection";
 
-const inter = Inter({ subsets: ["latin"] });
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+});
 
 export default function Home() {
   return (
-    <>
-      <Navbar />
+    <div className="w-[100vw] overflow-hidden">
+      <div className="w-[100vw] h-fit pb-16  bg-gradient-to-r from-[#5311A7] via-[#320F55] to-[#6E20D2]">
+        <Navbar />
+        <HeroSection />
+      </div>
+      <Carousel />
+      <ProjectsSection />
       <ServicesSection />
       <ProjectCollabSection />
       <FeaturesSection />
       <PaymentPlan />
       <ExpertsSection />
       <FooterSection />
-    </>
+    </div>
   );
 }
